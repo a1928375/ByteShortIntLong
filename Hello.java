9efca72b851bcc06487d5262c6c5e11e909fa986
Arthur Shih <a1928375@hotmail.com>
2020-03-27 17:46:44 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Arthur");
+
+        int myFirstNumber = (5*9)+3*6;
+
+        int mySecondNumber = 12;
+
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+
+        int myLastOne = 1000-myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+    }
+}
